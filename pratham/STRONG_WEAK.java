9dbde1df15691355b6057258f9a7ff66631a4f93
_WAR_MACHINE_ <you@example.com>
2018-11-13 17:02:30 +0530

    TO CHECK IF A PASSOWORD IS STRONG OR NOT

---
+++ pratham/STRONG_WEAK.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class STRONG_WEAK here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class STRONG_WEAK
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class STRONG_WEAK
+     */
+    public STRONG_WEAK()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
